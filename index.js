a33d2726258f2838ea4d23c700673066fa6f9c45
alamin1246 <hossainalamin1246@gmail.com>
2022-05-09 11:05:09 +0600

    add item api added

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+    // console.log('inside verifyJWT', authHeader);
+
+}
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.vmphq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -20,6 +38,16 @@ async function run() {
 
         const productCollection = client.db("kidsToysStock").collection("products");
 
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
+
         app.post("/uploadPd", async (req, res) => {
             const product = req.body;
             // console.log(product);
@@ -28,14 +56,14 @@ async function run() {
         });
 
         // products api
-        app.get('/product', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
 
-        app.get('/product/:id', async (req, res) => {
+        app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
@@ -43,20 +71,20 @@ async function run() {
         });
 
         // POST
-        app.post('/product', async (req, res) => {
+        app.post('/products', async (req, res) => {
             const newProduct = req.body;
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         });
 
-        /*   // DELETE
-          app.delete('/product/:id', async (req, res) => {
-              const id = req.params.id;
-              const query = { _id: ObjectId(id) };
-              const result = await productCollection.deleteOne(query);
-              res.send(result);
-          });
-   */
+        // DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
     } finally {
         // await client.close();
     }
